bfd55a6b2c9bc2ab32525076f7ae23f1b8f8d8ab
clair3st <clairejgatenby@gmail.com>
2024-10-23 09:50:16 -0700

    going back to text search box, adding axios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import { useState, useEffect} from 'react';
 import React from 'react';
-
-
+import axios from 'axios';
 import SimpleMap from './components/SimpleMap.jsx'
 
 
 function App() {
 	const [features, setFeatures] = useState([])
 	const [searchInput, setSearchInput] = useState('')
-
-	// useEffect(() => { 
-	// 	console.log("Mounted");
-	// 	getFacilities()
-	// },[]);
-
-
+	const [errorMessage, setErrorMessage] = useState('')
 
 	async function getFacilities(){
-		await fetch(`http://127.0.0.1:5000/api/v1/resources/features/${searchInput}`)
-			.then((response) => response.json())
-	  		.then((data) => {
-	    	setFeatures(data);
+		setErrorMessage('')
+
+		await axios.get(`http://127.0.0.1:5000/api/v1/resources/features/${searchInput}`)
+			.then((response) => {
+				if('data' in response){
+		    	setFeatures(response.data);
+
+				}
 	  })
 	  .catch((error) => {
 			console.error('Error fetching data:', error);
+			setErrorMessage('Something went wrong')
 	  });
 	}
 
@@ -42,22 +40,14 @@ function App() {
        <div className="container">
       <div >
         <form className="search-form" onSubmit={handleSubmit}>
-        <label htmlFor="parks">Choose a park feature:</label>
-
-				<select name="parks" id="pars" onChange={(e) => setSearchInput(e.target.value)}>
-				  <option value="basketball">Basketball</option>
-				  <option value="skate">Skateboard</option>
-				  <option value="boat">Boat Launch</option>
-				  <option value="cricket">Cricket</option>
-				  <option value="rugby">Rugby</option>
-				  <option value="soccer">Soccer</option>
-				  <option value="football">Football</option>
-				  <option value="tennis">Tennis Court</option>
-				</select>
+        <input type="text" placeholder="Enter a City" onChange={(e) => setSearchInput(e.target.value) } ></input>
           
           <button type="submit">
                 Explore!
            </button>
+           
+           <p className="error-message" style={ {'minHeight': '1.2em', color: 'red'}}>{errorMessage}</p>
+           
         </form>
       </div>
 
