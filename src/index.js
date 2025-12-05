5683984fe7934ac009e5a907e68176d7a0062cd4
clair3st <clairejgatenby@gmail.com>
2025-12-05 15:25:48 -0800

    Removed debug console.log Cleaned up formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-
-
-console.log('app')
 const root = createRoot(document.getElementById("root"));
 
-root.render(
- 
-        <App />
-
-);
+root.render(<App />);
