4160c5ea95aa05471e3427968b94141b624ae2c7
clair3st <clairejgatenby@gmail.com>
2025-12-05 15:25:33 -0800

    Removed unused mapRef and useRef import Extracted magic numbers to constants Fixed indentation Renamed x to feature for clarity Cleaned up JSX formatting

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -1,44 +1,44 @@
 import L from 'leaflet';
-import { useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+const SEATTLE_CENTER = [47.6061, -122.3328];
+const MAP_ZOOM = 13;
+const MAP_STYLE = { height: "80vh", width: "80vw" };
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
 });
 
-const SimpleMap = ({features}) => {
-  const mapRef = useRef(null);
-  const latitude = 47.6061;
-  const longitude = -122.3328;
-
-  return ( 
-  	<div>
-	  <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "80vh", width: "80vw"}}>
-		<TileLayer
-		  attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		  url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-		/>
-	  {features.map((x) => {
-	  		
-		  	const point =[parseFloat(x.ypos), parseFloat(x.xpos)]
-		  	return (
-		  		<Marker key={x.id} position={point}  icon={DefaultIcon} >
-		  			<Popup>
-				        {x.name}
-				        <br/>
-				        {x.feature_desc}
-				      </Popup>
-	            </Marker>
-		  	);
-		  }) 
-		}
-	  </MapContainer>
-	  </div>
+const SimpleMap = ({ features }) => {
+  return (
+    <div>
+      <MapContainer
+        center={SEATTLE_CENTER}
+        zoom={MAP_ZOOM}
+        style={MAP_STYLE}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        />
+        {features.map((feature) => {
+          const point = [parseFloat(feature.ypos), parseFloat(feature.xpos)];
+          return (
+            <Marker key={feature.id} position={point} icon={DefaultIcon}>
+              <Popup>
+                {feature.name}
+                <br />
+                {feature.feature_desc}
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </div>
   );
 };
 
